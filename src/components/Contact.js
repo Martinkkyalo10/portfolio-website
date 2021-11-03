fffbf3bce67badc34292b24443e811e61807811f
martin kyalo <martinkkyalo10@gmail.com>
2021-11-03 21:58:31 +0300

    this is the second commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { email } from '../data';
+import React from "react";
+import { email } from "../data";
 
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 export default function Articles() {
-    return (
-        <div className="contact container-fluid section" name="contact">
-            <Fade bottom>
-                <h2 className="section-header">CONTACT</h2>
-            </Fade>
-            <Fade bottom>
-                <p>Hit me up at <a href={"mailto:" + email} className="card-link">{email}</a> to enquire about collaboration.</p>
-            </Fade>
-        </div>
-    )
+  return (
+    <div className="contact container-fluid section" name="contact">
+      <Fade bottom>
+        <h2 className="section-header">CONTACT</h2>
+      </Fade>
+      <Fade bottom>
+        <p>
+          Hit me up at{" "}
+          <a href={"mailto:" + email} className="card-link">
+            {email}
+          </a>{" "}
+          or <br />
+          CALL: <span className="card-link">+254715836392</span> to enquire
+          about collaboration.
+        </p>
+      </Fade>
+    </div>
+  );
 }
